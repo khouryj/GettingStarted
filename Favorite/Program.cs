7a3c4af8d8efebb9bee6cc375538ac267a87d0e1
khouryj <khouryj6@gmail.com>
2017-08-31 11:26:42 -0400

    Favorite

---
+++ Favorite/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Favorite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite food: ");
+            String food = Console.ReadLine();
+            if (food == "pizza")
+            {
+                Console.WriteLine("You have great taste, good job!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong. Pizza is the greatest.");
+            }
+        }
+    }
+}
